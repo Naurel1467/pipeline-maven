efd98036dc69f473d00d0f871a3490d729ebdd99
Naurel1467 <ravitejamerugu@gmail.com>
2020-08-13 04:10:48 +0000

    modified PWD to WORKSPACE

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "********************"
+echo "***building jar****"
+echo "*******************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-maven-project1
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+echo "********************"
+echo "***testing****"
+echo "************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.me/:/root/.me/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-maven-project1
+
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.me/:/root/.me/ -w /app maven:3-alpine "$@"
