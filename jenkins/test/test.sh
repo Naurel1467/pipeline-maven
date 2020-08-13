12f247b460afee43bff64e8b7ef6378275816c23
Naurel1467 <ravitejamerugu@gmail.com>
2020-08-13 13:58:52 +0000

    modified java-app to java-appp

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -6,4 +6,4 @@ echo "************************"
 WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-maven-project1
 
 
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.me/:/root/.me/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-appp:/app -v /root/.me/:/root/.me/ -w /app maven:3-alpine "$@"
